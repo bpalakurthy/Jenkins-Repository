23a0823e32767a18c091f3a298c5655d4ca6ed88
root <root@ip-172-31-10-87.ec2.internal>
2022-04-27 00:19:06 +0000

    adding echo statements line

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo “thisisosmething”
 echo “thisis another thing”
-Adding a new line in the git bash
-adding a second line in the git bash
-added third line.
+echo Adding a new line in the git bash
+echo adding a second line in the git bash
+echo added third line.
